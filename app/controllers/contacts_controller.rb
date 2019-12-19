cdca89b6c35bb77820f7099e73b70b4e3c87606f
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-12-18 22:54:25 -0800

    css for mvp on admins index view. also added contact destroy method and link

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,6 @@
 class ContactsController < ApplicationController
   skip_before_action :verify_authenticity_token
+  before_action :find_contact, only: %i[destroy]
 
   def index
     if current_admin
@@ -20,9 +21,20 @@ class ContactsController < ApplicationController
     end
   end
 
+  def destroy
+    return unless @contact.destroy
+
+    redirect_to contacts_path
+    flash[:notice] = 'Location was successfully deleted'
+  end
+
   private
 
   def contact_params
     params.require(:contact).permit(:name, :email, :message)
+  end
+
+  def find_contact
+    @contact = Contact.find(params[:id])
   end
 end
