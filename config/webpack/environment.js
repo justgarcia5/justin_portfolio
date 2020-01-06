bc5f23a708459b80cdf60d19b0546381122b13b9
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2020-01-06 12:53:11 -0800

    heroku test

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,3 @@
 const { environment } = require('@rails/webpacker')
-const webpack = require("webpack")
-
-environment.plugins.append("Provide", webpack.ProvidePlugin({
-  $: 'Jquery',
-  JQuery: 'Jquery',
-  Popper: ['popper.js', 'default']
-}))
 
 module.exports = environment
