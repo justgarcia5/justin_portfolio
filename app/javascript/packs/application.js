fdd353c1c19a8a6f992b604acabdec43f7cc958e
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2020-01-06 11:40:14 -0800

    heroku test

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,18 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import "bootstrap"
+
+document.addEventListener("turbolinks:load", () => {
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="popover"]').popover()
+    $('#myModal').on('shown.bs.modal', function () {
+      $('#myInput').trigger('focus')
+    })
+  })
+})
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require("webpack")
+
+environment.plugins.append("Provide", webpack.ProvidePlugin({
+  $: 'Jquery',
+  JQuery: 'Jquery',
+  Popper: ['popper.js', 'default']
+}))
 
 module.exports = environment
