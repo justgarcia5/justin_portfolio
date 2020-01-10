06a532ea56bac0025ba6334c3243ab2ed9a84d47
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2020-01-10 12:04:18 -0800

    try heroku branch

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,5 +1,4 @@
 import React from "react"
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Intro from './Intro'
 import StickyBar from '../components/StickyBar'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,6 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -15,9 +10,16 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 // Support component names relative to this directory:
-import "bootstrap";
 
-import "../packs/stylesheets/application";
+import "bootstrap";
+// import "@fortawesome/fontawesome-free/js/all";
+import "./stylesheets/application.scss";
+
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
 
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
