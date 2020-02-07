b0ed1db9f67d9f119814183a4fadc1334b5eeaa9
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2020-02-07 15:00:48 -0800

    heroku fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,17 +3,21 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import 'bootstrap'
+import './stylesheets/application.scss'
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
-//
+
 const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
-// Support component names relative to this directory:
 
-import "bootstrap";
-// import "@fortawesome/fontawesome-free/js/all";
-import "./stylesheets/application.scss";
 
 require("bootstrap/dist/js/bootstrap")
 require("@rails/ujs").start()
@@ -27,5 +31,4 @@ ReactRailsUJS.useContext(componentRequireContext);
 
 document.addEventListener("turbolinks:load", () => {
   $('[data-toggle="tooltip"]').tooltip()
-  // console.log("this is webpack")
 })
