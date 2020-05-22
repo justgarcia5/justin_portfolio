1be876bee8f020528b34f0c7f2bba2f76e0f7d86
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2020-05-22 14:02:35 -0700

    changed font size for name

--- app/javascript/components/IntoText.js
+++ app/javascript/components/IntoText.js
@@ -5,7 +5,7 @@ function introText() {
     <div className="star-wars">
       <div className="container crawl">
         <div className="title">
-          <h1 className="text-center">JUSTIN GARCIA</h1>
+          <h1 className="intro-name text-center">JUSTIN GARCIA</h1>
           <h1 className="text-center">WEB DEVELOPER</h1>
           <p>I am a web developer with two years experience building and creating responsive and high quality applications using React and Ruby on Rails... </p>
         </div>
