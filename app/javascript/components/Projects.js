29e20cb06a369cf68f7e1ea9cc239d9e1dcc56be
justingarcia <justgarcia5@gmail.com>
2020-07-03 12:02:31 -0700

    added wordpress project

--- app/javascript/components/Projects.js
+++ app/javascript/components/Projects.js
@@ -7,18 +7,20 @@ import TradeUp from '../components/projects/trade_up'
 import Responsive from '../components/projects/responsive'
 import TechTalk from '../components/projects/tech_talk'
 import Overtime from '../components/projects/overtime'
+import Surfshop from '../components/projects/surfshop'
 
 export default function Projects() {
   return (
     <div className="">
       <div className="projects-div" name="projects" id="projects">
-        {/* <Fade right delay={200}> */}
+        <Fade right >
           <h2 className="projects-h2 text-center">My Work</h2>
           <hr className="col-sm-4"/>
-        {/* </Fade> */}
+        </Fade>
         <div className="d-flex justify-content-center mt-5">
-          {/* <Fade left delay={200}> */}
+          <Fade left >
             <div className="row justify-content-center">
+              <Surfshop styles={styles} class="project-cards"/>
               <TradeUp styles={styles} class="project-cards"/>
               <Overtime styles={styles} class="project-cards" />
               <RecipeApp styles={styles} class="project-cards" />
@@ -27,7 +29,7 @@ export default function Projects() {
               <Responsive styles={styles} class="project-cards" />
               <TechTalk styles={styles} class="project-cards" />
             </div>
-          {/* </Fade> */}
+          </Fade>
         </div>
       </div>
     </div>
---
+++ app/javascript/components/projects/surfshop.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import { Modal, Button } from 'react-bootstrap';
+
+import SurfshopImage from '../../images/projects/surfshop.png'
+
+export default function Surfshop(props) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <div className="card-col m-5">
+      <div className="card hovereffect" >
+        <img src={SurfshopImage} className="card-img-top" height={280}  alt="..."/>
+        <div className="overlay">
+          <div className="d-flex justify-content-center mt-5">
+            <Button className="btn btn-success mx-2 mt-5" onClick={handleShow}>
+              Learn More
+            </Button>
+            <Button className="btn btn-info mx-2 mt-5" href="https://www.mysurfshopstore.com/" target="_blank">
+              Live Demo
+            </Button>
+            <Modal
+              show={show}
+              onHide={handleClose}
+              size="lg"
+              id="modal"
+            >
+              <Modal.Header>
+                <Modal.Title >MYSURFSHOPSTORE</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <h5>Date:</h5>
+                <p className="ml-4">June 2016 - Current</p>
+                <h5>Description:</h5>
+                <p className="ml-4">This is MYSURFSHOPSTORE which is built on Bitnami Wordpress and uses WooComerce. At the moment this project is still a work in progress.</p>
+                <h5>My Contributions:</h5>
+                <ul>
+                  <li>Created AwS EC2 Instance with Wordpress Bitnami</li>
+                  <li>Added WooCommerce plugin and connected stripe account for payments</li>
+                  <li>Implemented custom UI Theme</li>
+                </ul>
+                <h5>Technologies:</h5>
+                <p className="ml-4">Wordpress, WooComerce, php, AWS</p>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button className="btn btn-info mx-2" href="https://www.mysurfshopstore.com/" target="_blank">
+                  Live Demo
+                </Button>
+                <Button variant="secondary" onClick={handleClose}>
+                  Close [X]
+                </Button>
+              </Modal.Footer>
+            </Modal>
+
+          </div>
+        </div>
+        <div className="card-body">
+          <h5 className="card-title mt-4">MYSURFSHOPSTORE</h5>
+        </div>
+      </div>
+    </div>
+  )
+}
