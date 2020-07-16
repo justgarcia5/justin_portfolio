8dec23a8c75022ef69c3159c6282af6a5ccd5710
justingarcia <justgarcia5@gmail.com>
2020-07-16 12:23:05 -0700

    added aws image

--- app/javascript/components/About.js
+++ app/javascript/components/About.js
@@ -1,18 +1,20 @@
 import React from "react"
 
+import aws from '../images/aws.png'
+
 function About() {
 
   return (
     <div>
       <div className="about container" name="about" id="about" method="post">
-        <div className="row d-flex">
+        <div className="row d-flex mb-5">
           <div className="col-sm mt-5">
             <h2 className="about-title">About</h2>
             <hr/>
             <p className="mt-5 text-center">I am a web developer with two years experience building and creating responsive and high quality applications using React and Ruby on Rails. I have obtained a solid foundation and understanding of web development while attending and completing a Full-Stack bootcamp. Thereafter the bootcamp, I was an intern for a consultant company, Coding Zeal. During this time I gained experience and knowledge in working in an Agile Software Development process.</p>
           </div>
           <div className="col-sm mt-5">
-          <h2 className="about-title">Skills</h2>
+            <h2 className="about-title">Skills</h2>
             <hr/>
             <div className="mt-5">
               <p>JavaScript</p>
@@ -27,7 +29,12 @@ function About() {
             </div>
           </div>
           <div className="col-sm mt-5">
-          <iframe src="https://open.spotify.com/embed/album/0RkRavz2FtCFuFe3U9iEZE" width="340" height="480" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+            <iframe src="https://open.spotify.com/embed/album/0RkRavz2FtCFuFe3U9iEZE" width="340" height="480" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+          </div>
+        </div>
+        <div className="row d-flex">
+          <div className="col-sm mt-5">
+            <img src={aws}/>
           </div>
         </div>
       </div>
