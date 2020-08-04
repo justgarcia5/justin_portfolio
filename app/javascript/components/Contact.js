698ac9ee3249f264c682a7ad15ef4da79bebeb01
justingarcia <justgarcia5@gmail.com>
2020-08-04 16:28:39 -0700

    updated icons and links

--- app/javascript/components/Contact.js
+++ app/javascript/components/Contact.js
@@ -127,11 +127,11 @@ class Contact extends React.Component {
         </form>
         <footer>
           <div className="icons d-flex justify-content-center">
-            <a href="https://www.facebook.com/justin.garcia.378199" className='text-decoration-none icon-foot p-2' target="_blank"><i className="fa fa-facebook"></i></a>
-            <a href="https://twitter.com/Justgarcia85" className='text-decoration-none icon-foot p-2' target="_blank"><i className="fa fa-twitter"></i></a>
-            <a href="https://github.com/justgarcia5" className='text-decoration-none icon-foot p-2' target="_blank"><i className="fa fa-github"></i></a>
-            <a href="https://www.linkedin.com/in/just-g-garcia/" className='text-decoration-none icon-foot p-2' target="_blank"><i className="fa fa-linkedin"></i></a>
-            <a href="mailto:justgarcia5@gmail.com" className='text-decoration-none icon-foot p-2' target="_blank"><i className="fa fa-envelope"></i></a>
+            <a href="https://www.facebook.com/justin.garcia.378199" className='icon-foot p-2' target="_blank"><i className="fa fa-facebook"></i></a>
+            <a href="https://codepen.io/Freshmex" className='icon-foot p-2' target="_blank"><i className="fa fa-codepen"></i></a>
+            <a href="https://github.com/justgarcia5" className='icon-foot p-2' target="_blank"><i className="fa fa-github"></i></a>
+            <a href="https://www.linkedin.com/in/just-g-garcia/" className='icon-foot p-2' target="_blank"><i className="fa fa-linkedin"></i></a>
+            <a href="mailto:justgarcia5@gmail.com" className='icon-foot p-2' target="_blank"><i className="fa fa-envelope"></i></a>
           </div>
           <br/>
           <p><a href="mailto:justgarcia5@gmail.com">Justin Garcia</a> © 2020 | Culver City, Ca</p>
