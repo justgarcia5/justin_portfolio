e98eb32e52cc4b4a0f1d57fdcb3f3c316d89fa85
JUSTGARCIA5 <JUSTGARCIA5@GMAIL.COM>
2022-05-24 19:36:03 -0700

    fist

--- app/javascript/components/Projects.js
+++ app/javascript/components/Projects.js
@@ -24,7 +24,6 @@ export default function Projects() {
           <Fade up >
             <div className="row justify-content-center">
               {/* <Exercise styles={styles} class="project-cards" /> */}
-
               <Meditation styles={styles} class="project-cards" />
               {/* <Surfshop styles={styles} class="project-cards"/> */}
               <TradeUp styles={styles} class="project-cards"/>
